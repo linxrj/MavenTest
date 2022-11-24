7b441f2ac65d7509a98fbf22af32e104ccdf191b
Jonas Röhrle <jo.roehrle@hotmail.com>
2022-11-24 12:21:06 +0100

    all maven files, except target directory

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,13 @@
+package de.hfu;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner scanner = new Scanner(System.in);
+    	String input = scanner.nextLine();
+    	System.out.println(input.toUpperCase());
+    }
+}
