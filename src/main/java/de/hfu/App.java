39fbd513f8179c55284455d40f580fdbc7ee511d
Jonas Röhrle <jo.roehrle@hotmail.com>
2022-11-24 12:40:24 +0100

    added javadoc-comments

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -2,8 +2,19 @@ package de.hfu;
 
 import java.util.Scanner;
 
+/**
+* App zum Einlesen eines Strings und der Ausgabe
+* desselben Strings in Großschreibung.
+*
+* @author Jonas
+* @version 1.0
+*/
 public class App 
 {
+	/**
+	 * main-Methode, welche die Funktionalität ausführt.
+	 * @param args
+	 */
     public static void main( String[] args )
     {
     	Scanner scanner = new Scanner(System.in);
